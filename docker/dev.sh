bb8a17f1c04a002132d06d8ac8deacd93fd00adf
QED0711 <qdizon@gmail.com>
2023-09-28 06:46:20 -0400

    working sma transformer

--- docker/dev.sh
+++ docker/dev.sh
@@ -2,6 +2,7 @@
 
 USERNAME=$(whoami)
 
-docker run --rm -ti \
+docker run --rm -ti -d \
+    --name tr8tr_rs_dev \
     -v $PWD/:/home/$USERNAME/app \
-    tr8tr-rs-dev:latest bash
+    tr8tr-rs-dev:latest tail -f /dev/null
