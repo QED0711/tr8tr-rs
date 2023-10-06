dee0c6f5e04bbd66a0639000124291ddfa8dd75e
QED0711 <qdizon@gmail.com>
2023-10-06 01:27:34 +0000

    added band struct to trigger

--- src/modules/triggers/sr_bounce.rs
+++ src/modules/triggers/sr_bounce.rs
@@ -2,7 +2,13 @@ use polars::prelude::PolarsError;
 
 use crate::modules::{trigger::{Trigger, TriggerResponse}, asset::Asset};
 
+struct PriceBand {
+    center: f64,
+    min: f64,
+    max: f64,
+}
 
+#[allow(non_snake_case)]
 pub fn WEEKLY_PIVOT_BOUNCE() -> Trigger {
 
     fn weekly_pivot_bounce_executor(asset: &Asset) -> Result<TriggerResponse, PolarsError> {
@@ -11,6 +17,14 @@ pub fn WEEKLY_PIVOT_BOUNCE() -> Trigger {
             Some(symbol) => if symbol.contains("JPY")  {0.01} else {0.0001}
             None => 0.0001
         };
+
+        let band_pip_rng = 8.;
+
+        let df = asset.df.as_ref().unwrap();
+        let cur = df.tail(1);
+
+        let pivot_point = cur.column("pivot_point").unwrap().f64().unwrap().get(0).unwrap().unwrap();
+        let pivot_band: PriceBand = PriceBand{center: pivot_point, min: pivot_point - (pip_value * band_pip_rng), max: pivot_point + (pip_value * band_pip_rng)};
 
 
         Ok(TriggerResponse {
